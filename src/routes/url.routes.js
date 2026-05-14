b6e535a8e43da4538d4d33f8d3b47446de6bbcd4
jpvamja <jpvamja@gmail.com>
2026-05-14 11:13:31 +0530

    feat: add rate limiting middleware for URL creation and redirection endpoints

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import rateLimit from "express-rate-limit";
+
 import asyncHandler from "../utils/asyncHandler.js";
 
 import {
@@ -8,20 +10,51 @@ import {
     detailsOfShortUrl,
 } from "../controllers/url.controller.js";
 
+const createShortUrlLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 20, 
+    message: {
+        success: false,
+        message: "Too many short URLs created. Please try again later.",
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const redirectToOriginalUrlLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 300,
+    message: "Too many redirect requests.",
+});
+
+const detailsOfShortUrlLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 60,
+    message: {
+        success: false,
+        message: "Too many requests for details API.",
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 const router = express.Router();
 
 router.post(
     "/shortener",
+    createShortUrlLimiter,
     asyncHandler(createShortUrl)
 );
 
 router.get(
     "/:shortUrl",
+    redirectToOriginalUrlLimiter,
     asyncHandler(redirectToOriginalUrl)
 );
 
 router.get(
     "/details/:shortUrl",
+    detailsOfShortUrlLimiter,
     asyncHandler(detailsOfShortUrl)
 );
 
